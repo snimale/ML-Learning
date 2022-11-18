e59a41c396d6b15382a4496f511ae17212c2cecc
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-11-18 17:30:27 +0530

    added point and vector - 1

--- src/characterMovements/Vector.java
+++ src/characterMovements/Vector.java
@@ -1,5 +1,34 @@
 package characterMovements;
 
-public class Vector {
+class Point {
+	double x;
+	double y;
+	public Point(double x, double y) {
+		this.x=x;
+		this.y=y;
+	}
+}
 
+public class Vector {
+	double i;
+	double j;
+	public double length() {
+		return Math.sqrt(i*i + j*j);
+	}
+	public double lengthSq() {
+		return i*i + j*j; // to compare distance no need sqrt 
+	}
+	public void scale(double n) {
+		i*=n;
+		j*=n;
+	}
+	public Vector(double x, double y) {
+		this.i=x;
+		this.j=y;
+	}
+	public Vector (Point from, Point to) {
+		this.i=to.x-from.x;
+		this.j=to.y-from.y;
+	}
+	
 }
