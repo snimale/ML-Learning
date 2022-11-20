3d68ec9d4c47f37d05bf0280c2678e964d69041c
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-11-20 18:19:02 +0530

    added third dimension to vector, point && method normalize, add

--- src/characterMovements/Vector.java
+++ src/characterMovements/Vector.java
@@ -3,32 +3,51 @@ package characterMovements;
 class Point {
 	double x;
 	double y;
-	public Point(double x, double y) {
+	double z;
+	public Point(double x, double y, double z) {
 		this.x=x;
 		this.y=y;
+		this.z=z;
 	}
 }
 
 public class Vector {
 	double i;
 	double j;
+	double k;
 	public double length() {
-		return Math.sqrt(i*i + j*j);
+		return Math.sqrt(i*i + j*j+ k*k);
 	}
 	public double lengthSq() {
-		return i*i + j*j; // to compare distance no need sqrt 
+		return i*i + j*j + k*k; // to compare distance no need sqrt 
 	}
 	public void scale(double n) {
 		i*=n;
 		j*=n;
+		k*=n;
 	}
-	public Vector(double x, double y) {
+	public static Vector normalize(Vector v) { // to normalize -> to convert vector to unit vector
+		return new Vector(v.i/v.length(), v.j/v.length(), v.k/v.length());
+	}
+	public static Vector add(Vector a, Vector b) {
+		return new Vector(a.i+b.i, a.j+b.j, a.k+b.k);
+	}
+	
+	// constructor
+	public Vector(double x, double y, double z) {
 		this.i=x;
 		this.j=y;
+		this.k=z;
 	}
 	public Vector (Point from, Point to) {
 		this.i=to.x-from.x;
 		this.j=to.y-from.y;
+		this.k=to.z-from.z;
+	}
+	public Vector (Vector v) {
+		this.i=v.i;
+		this.j=v.j;
+		this.k=v.k;
 	}
 	
 }
